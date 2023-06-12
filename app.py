d5774c556cef2e286aedbfb66556891a69ab0047
Fred <fredkesse1234@gmail.com>
2023-06-12 17:18:07 +0000

    ajout des liens

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from mongoengine import connect
 from routes.user import route_utilisateur
 
 app = Flask(__name__)
-# connect("mongodb+srv://fredkesse1234:9LTahJJoOYPUyn5T@cluster2.0zjv9er.mongodb.net")
-connect( db='Transport', username='user', password='9LTahJJoOYPUyn5T', host='mongodb://user:9LTahJJoOYPUyn5T@cluster2.0zjv9er.mongodb.net/transport')
+connect(host="mongodb+srv://fredkesse1234:4f7E5YpybxrOqAlp@cluster0.qzxcffy.mongodb.net/")
+# 4f7E5YpybxrOqAlp
+# connect( db='Transport', username='user', password='9LTahJJoOYPUyn5T', host='mongodb://user:9LTahJJoOYPUyn5T@cluster2.0zjv9er.mongodb.net/transport')
 
 app.register_blueprint(route_utilisateur, url_prefix='/api/user')
 
