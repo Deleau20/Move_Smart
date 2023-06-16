9d8c74eed5175adaab010479a23686f59d6cef27
Fred <fredkesse1234@gmail.com>
2023-06-16 17:13:16 +0000

    LA SESSION NE SE DECONNECTE PAS

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ connect(host="mongodb+srv://fredkesse1234:4f7E5YpybxrOqAlp@cluster0.qzxcffy.mong
 # 4f7E5YpybxrOqAlp
 # connect( db='Transport', username='user', password='9LTahJJoOYPUyn5T', host='mongodb://user:9LTahJJoOYPUyn5T@cluster2.0zjv9er.mongodb.net/transport')
 
-app.register_blueprint(route_utilisateur, url_prefix='/api/user')
+app.register_blueprint(route_utilisateur, url_prefix='/')
 
 
     
--- routes/user.py
+++ routes/user.py
@@ -1,18 +1,49 @@
-from flask import Flask, render_template, request, Blueprint, flash, redirect, url_for
-from flask import jsonify, request
+from flask import Flask, render_template, request, Blueprint, flash, redirect, url_for, session, jsonify
 from models.user import Utilisateur
 from passlib.hash import sha256_crypt
 import re
+from bson import ObjectId
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fredkesse1234'
 
-route_utilisateur = Blueprint('user_router', __name__, url_prefix='/api/user')
+
+def jsonify_with_objectid_support(data):
+    def convert(obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        raise TypeError(
+            f"Object of type {type(obj).__name__} is not JSON serializable")
+
+    return json.dumps(data, default=convert)
+
+
+def get_current_user():
+    user_id = session.get('user_id')
+    if user_id:
+        # Remplacez cette ligne avec la méthode appropriée pour récupérer l'utilisateur
+        utilisateur = Utilisateur.objects(id=user_id)
+        return utilisateur[0]
+    return None
+
+
+route_utilisateur = Blueprint('user_router', __name__, url_prefix='/')
 
 
 @route_utilisateur.route('/', methods=['GET', 'POST'])
 def accueil():
     return render_template('index.html')
+
+
+@route_utilisateur.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+
+@route_utilisateur.route('/contact', methods=['GET'])
+def contact():
+    return render_template('contact.html')
 
 
 @route_utilisateur.route('/showlogin', methods=['GET'])
@@ -22,24 +53,21 @@ def showlogin():
 
 @route_utilisateur.route('/inscription', methods=['POST'])
 def inscription():
-    # sha256_crypt.verify()
     try:
         prenom = request.form['prenom']
         nom = request.form['nom']
         email = request.form['email']
         mot_de_passe = request.form['mot_de_passe']
 
-        # Vérifier que le nom et le prénom ne contiennent pas de caractères spéciaux
         if not re.match(r'^[a-zA-Z\s]+$', prenom) or not re.match(r'^[a-zA-Z\s]+$', nom):
-            flash('Le nom et le prénom ne doivent contenir que des lettres et des espaces.', 'warning')
-            return redirect(url_for('user_router.showlogin'))
-        
-        # Vérifier que le mot de passe a une longueur supérieure à 8 caractères
-        if len(mot_de_passe) <= 8:
-            flash('Le mot de passe doit contenir plus de 8 caractères.', 'warning')
+            flash(
+                'Le nom et le prénom ne doivent contenir que des lettres et des espaces.', 'warning')
             return redirect(url_for('user_router.showlogin'))
 
-        # Hacher le mot de passe
+        if len(mot_de_passe) <= 6:
+            flash('Le mot de passe doit contenir plus de 6 caractères.', 'warning')
+            return redirect(url_for('user_router.showlogin'))
+
         mot_de_passe_hache = sha256_crypt.encrypt(mot_de_passe)
 
         utilisateur = Utilisateur(
@@ -48,11 +76,12 @@ def inscription():
 
         return redirect(url_for('user_router.showconnect'))
     except Exception as e:
-        return e.__str__()
+        return str(e)
 
 
 @route_utilisateur.route('/showconnect', methods=['GET'])
 def showconnect():
+    print("Connect :", session.get('user_id'))
     return render_template('connexion.html')
 
 
@@ -64,18 +93,78 @@ def connexion():
         utilisateur = Utilisateur.objects(email=email).first()
 
         if utilisateur and sha256_crypt.verify(mot_de_passe, utilisateur.mot_de_passe):
+            session['user_id'] = str(utilisateur["id"])
             flash('Connexion réussie !', 'success')
-            return render_template('accueil.html')
+            return redirect(url_for('user_router.accueilU'))
         else:
             flash('Email ou mot de passe incorrect !', 'danger')
             return redirect(url_for('user_router.showconnect'))
 
     except Exception as e:
-        flash("Une erreur s'est produite lors de la connexion.', 'danger")
+        flash("Une erreur s'est produite lors de la connexion.", 'danger')
+        print(e)
         return redirect(url_for('user_router.showconnect'))
+
+
+@route_utilisateur.route('/accueilU', methods=['GET'])
+def accueilU():
+    if not session.get('user_id'):
+        return redirect(url_for('user_router.showconnect'))
+    if get_current_user():
+        user = get_current_user()
+        return render_template("accueil_user.html")
+    else:
+        return redirect(url_for('user_router.showconnect'))
+
+
+@route_utilisateur.route('/aboutU', methods=['GET'])
+def aboutU():
+    if get_current_user():
+        user = get_current_user()
+        return render_template('about_user.html')
+    else:
+        return redirect(url_for('user_router.showconnect'))
+    
+
+
+@route_utilisateur.route('/alertU', methods=['GET'])
+def alertU():
+    if get_current_user():
+        user = get_current_user()
+        return render_template('alert_user.html')
+    else:
+        return redirect(url_for('user_router.showconnect'))
+    
+
+
+@route_utilisateur.route('/incidentsU', methods=['GET'])
+def incidentsU():
+    if get_current_user():
+        user = get_current_user()
+        return render_template('incidents_user.html')
+    else:
+        return redirect(url_for('user_router.showconnect'))
+
+
+@route_utilisateur.route('/contactU', methods=['GET'])
+def contactU():
+        if get_current_user():
+            user = get_current_user()
+            return render_template('contact_user.html')
+        else:
+            return redirect(url_for('user_router.showconnect'))
+    
+
+
+@route_utilisateur.route('/deconnexion', methods=['GET'])
+def deconnexion():
+    print("Deconnexion :", session.get('user_id'))
+    session.pop('user_id', None)
+    flash('Vous avez été déconnecté.', 'success')
+    return redirect(url_for('user_router.showconnect'))
 
 
 app.register_blueprint(route_utilisateur)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
