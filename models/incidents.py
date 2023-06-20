1d8034d1af6b2955adb22b0f0c9d83d89154102a
Fred <fredkesse1234@gmail.com>
2023-06-20 17:46:57 +0000

    ajout de localisation

--- models/incidents.py
+++ models/incidents.py
@@ -4,8 +4,6 @@ from mongoengine import Document, StringField, ImageField, PointField
 class Alerte(Document):
     prenom = StringField(required=True)
     nom = StringField(required=True)
-    email = StringField(required=True, unique=True)
-    mot_de_passe = StringField(required=True)
     image = ImageField()
     localisation = PointField()
     titre = StringField(required=True)
